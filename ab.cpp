d6a9a23ef137652f8ab6078e2f87c192f6a7822f
Kunal Singh Rana <kunalsinghrana@Kunals-MacBook-Air.local>
2021-11-05 12:56:32 +0530

    "fifth commit"

--- ab.cpp
+++ ab.cpp
@@ -5,9 +5,6 @@ int main(){
     cout<<"hello world"<<endl;
     cout<<"I am learning git"<<endl;
     cout<<"I am very excited"<<endl;
-    int a;
-    int b;
-    cin>>a>>b;
-    cout<<(a+b)<<" "<<(a*b)<<endl;
+    
     return 0;
 }
