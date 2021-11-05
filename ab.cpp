733d280c56607ec6bbeb38b2b186a36a8e971831
Kunal Singh Rana <kunalsinghrana@Kunals-MacBook-Air.local>
2021-11-05 12:49:03 +0530

    "adding operators"

--- ab.cpp
+++ ab.cpp
@@ -5,5 +5,9 @@ int main(){
     cout<<"hello world"<<endl;
     cout<<"I am learning git"<<endl;
     cout<<"I am very excited"<<endl;
+    int a;
+    int b;
+    cin>>a>>b;
+    cout<<(a+b)<<" "<<(a*b)<<endl;
     return 0;
 }
